303d5357587c3a1b951fbc4e21172586ded791a0
Alain Richardt <alain@atomictessellator.com>
2024-11-04 09:32:28 +1300

    pyproject for distribution

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 from setuptools import setup, find_packages
-from ataims.version import __version__
-
-
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    install_requires = [line.strip() for line in fh.readlines() if line.strip()]
 
 setup(
-    name='ataims',
-    version=__version__,
     packages=find_packages(),
-    description='A parser for FHI-aims output files',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/AtomicTessellator/at-aims',
-    install_requires=install_requires,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.10',
-    entry_points={
-    'console_scripts': [
-        'ataims=ataims.__main__:main',
-        ],
-    },
 )
